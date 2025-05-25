1253a1b5d699dd336a3b7aedfd0723bda4b5c1f5
Tiago Salvatierra <tiagosalvatierra258@gmail.com>
2025-05-25 12:05:09 -0300

    remove a useless container in the structure

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,10 @@
 export default function Card({ index, onClick, image, name }) {
   return (
-    <div className="character-card-container">
-      <button onClick={() => onClick(index)} className="character-card">
-        <div className="character-card-frame">
-          <img className="character-card-img" src={image} alt={name} />
-          <p className="character-card-name">{name}</p>
-        </div>
-      </button>
-    </div>
+    <button onClick={() => onClick(index)} className="character-card">
+      <div className="character-card-frame">
+        <img className="character-card-img" src={image} alt={name} />
+        <p className="character-card-name">{name}</p>
+      </div>
+    </button>
   );
 }
