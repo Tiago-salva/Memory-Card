9f0d6ea2a9311a667740672e52c1eb8f98eeb853
Tiago Salvatierra <tiagosalvatierra258@gmail.com>
2025-05-25 12:05:54 -0300

    add a component to show the title and the different difficulty btns

---
+++ src/components/Home.jsx
@@ -0,0 +1,51 @@
+import { useEffect, useState } from "react";
+
+export default function Home({ setGameStarted, setDifficulty }) {
+  const [showTitle, setShowTitle] = useState(false);
+  const difficulties = [
+    {
+      id: 0,
+      className: "easy",
+      difficulty: "Easy",
+    },
+    {
+      id: 1,
+      className: "medium",
+      difficulty: "Medium",
+    },
+    {
+      id: 2,
+      className: "hard",
+      difficulty: "Hard",
+    },
+  ];
+
+  function startGame(difficulty) {
+    setDifficulty(difficulty);
+    setGameStarted(true);
+  }
+
+  useEffect(() => {
+    setShowTitle(true);
+  }, []);
+
+  return (
+    <div className="home-container">
+      <img
+        src="/src/img/title.png"
+        className={showTitle ? "home-title-displayed" : "home-title"}
+      ></img>
+      <div className="difficulty-container">
+        {difficulties.map((btn) => (
+          <button
+            key={btn.id}
+            onClick={() => startGame(btn.difficulty)}
+            className={`difficulty-btn-${btn.className}`}
+          >
+            {btn.difficulty}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+}
