858777a9699a9c5a5b2f9aca3933eacb655ea417
Tiago Salvatierra <tiagosalvatierra258@gmail.com>
2025-05-26 13:48:00 -0300

    create a modal component to display a message, last score and a restart btn

---
+++ src/components/Modal.jsx
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+
+export default function Modal({ gameFinished, message, onClick, score }) {
+  return (
+    <div className={"end-game-modal-container"}>
+      <div
+        className={
+          gameFinished ? "end-game-modal-displayed" : "end-game-modal-hidden"
+        }
+      >
+        <h2 className="modal-title">{message}</h2>
+        <p className="modal-final-score">{`Your final score is: ${score}`}</p>
+        <img
+          src="/src/img/chicken.png"
+          className="chicken-img"
+          alt="chicken"
+        ></img>
+        <button className="modal-btn" onClick={() => onClick()}>
+          Restart
+        </button>
+      </div>
+    </div>
+  );
+}
