a2cc808f022dee4239df9364f88cb3f5922dfeba
Tiago Salvatierra <tiagosalvatierra258@gmail.com>
2025-05-26 13:45:22 -0300

    add different classes to control the animations

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,9 @@
-export default function Card({ index, onClick, image, name }) {
+export default function Card({ index, onClick, image, name, newCharacter }) {
   return (
-    <button onClick={() => onClick(index)} className="character-card">
+    <button
+      onClick={() => onClick(index)}
+      className={newCharacter ? "character-card-front" : "character-card-inner"}
+    >
       <div className="character-card-frame">
         <img className="character-card-img" src={image} alt={name} />
         <p className="character-card-name">{name}</p>
